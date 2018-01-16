d801968d1d81ff16a6ed248f77f09b67543763e3
Anoop M <noop2016@gmail.com>
2018-01-16 21:11:08 +0000

    added respective files and updated css

---
+++ web_dynamic/2-hbnb.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Flask app to generate complete html page containing location/amenity
+dropdown menus and rental listings"""
+import uuid
+from flask import Flask, render_template
+from flask_cors import CORS
+from models import storage
+app = Flask('web_dynamic')
+app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+
+
+@app.route('/2-hbnb/')
+def display_hbnb():
+    """Generate page with popdown menu of states/cities"""
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    places = storage.all('Place')
+    cacheId = str(uuid.uuid4())
+    return render_template('2-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cacheId)
+
+
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    """Close database or file storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  let amenityFilter = [];
+  $('div.amenities input:checkbox').change(
+    function () {
+      if ($(this).is(':checked')) {
+        amenityFilter.push(' ' + $(this).attr('data-name'));
+      } else amenityFilter.pop(' ' + $(this).attr('data-name'));
+      $('div.amenities h4').text(amenityFilter);
+    });
+});
